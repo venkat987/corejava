751269df1324b5ff74f146a316118602caf65b8e
Venkat K <venkateshan85@gmail.com>
2022-12-29 20:02:11 -0800

    InterFaceDemo

---
+++ ex07_Classes/src/com/monika/InterFaceDemo.java
@@ -0,0 +1,33 @@
+package com.monika;
+
+public class InterFaceDemo {
+	
+	// interface ( example man and machine , between 
+	public static void main(String[] arg) {
+		Cascio c1 = new Cascio();
+		c1.add();
+		c1.print();
+		c1.sound();
+		System.out.println(c1.price);
+	}
+}
+
+interface Calculator{
+	int price = 100; // By default its final  
+	void add();
+	void print();
+}
+
+class Cascio implements Calculator {
+	@Override
+	public void add() {
+		System.out.println("Adding");
+	}
+	@Override
+	public void print() {
+		System.out.println("Printing");
+	}
+	public void sound() {
+		System.out.println("Beep");
+	}
+}
--- ex07_Classes/src/com/monika/StaticClassDemo.java
+++ ex07_Classes/src/com/monika/StaticClassDemo.java
@@ -5,7 +5,10 @@ public class StaticClassDemo {
 	 	Hotel.Veg v1 = new Hotel.Veg();
 	 	v1.kitchen();
 	 	
+	 	Marks.Maths m1 = new Marks.Maths();
+	 	m1.maths();
 	}
+	
 }
 
 class Hotel{
@@ -15,3 +18,20 @@ class Hotel{
 		}
 	}
 }
+class Marks{
+	static class Maths{
+		void maths() {
+			int m = 40;
+			if (m>=40) {
+			System.out.println("pass =" + m);
+			} else {
+				System.out.println("fail");
+			  }
+			do {
+				System.out.println("fail");
+			} while(m<40);
+			m--;
+			}
+			}
+		}
+	
